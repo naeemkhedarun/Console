3a2e69445fa7f8b68f01437546be25ba845d72bf
Paul Marston <vai@vai-net.co.uk>
2013-08-05 02:34:04 +0100

    Many changes mid way through dev but tired...

--- Install/ConEmu Shell Integration.ps1
+++ Install/ConEmu Shell Integration.ps1
@@ -2,35 +2,35 @@ param($InstallPath)
 
 "Configuring shell integration (ConEmu)..."
 New-Item "HKCU:\Software\Classes\*\shell\Open ConEmu" -Force | Out-Null
-New-ItemProperty "HKCU:\Software\Classes\*\shell\Open ConEmu" "Icon" -Value "$InstallPath\Icons\PowerShellPrompt.ico" -Type String -Force | Out-Null
+New-ItemProperty "HKCU:\Software\Classes\*\shell\Open ConEmu" "Icon" -Value "$InstallPath\Install\Icons\PowerShellPrompt.ico" -Type String -Force | Out-Null
 New-Item "HKCU:\Software\Classes\*\shell\Open ConEmu\command" -Value """$InstallPath\ConEmu\ConEmu.exe"" /cmd powershell.exe" -Type String -Force | Out-Null
 
 New-Item "HKCU:\Software\Classes\Directory\Background\shell\Open ConEmu" -Force | Out-Null
-New-ItemProperty "HKCU:\Software\Classes\Directory\Background\shell\Open ConEmu" "Icon" -Value "$InstallPath\Icons\PowerShellPrompt.ico" -Type String -Force | Out-Null
+New-ItemProperty "HKCU:\Software\Classes\Directory\Background\shell\Open ConEmu" "Icon" -Value "$InstallPath\Install\Icons\PowerShellPrompt.ico" -Type String -Force | Out-Null
 New-Item "HKCU:\Software\Classes\Directory\Background\shell\Open ConEmu\command" -Value """$InstallPath\ConEmu\ConEmu.exe"" /cmd powershell.exe" -Type String -Force | Out-Null
 
 New-Item "HKCU:\Software\Classes\Directory\shell\Open ConEmu" -Force | Out-Null
-New-ItemProperty "HKCU:\Software\Classes\Directory\shell\Open ConEmu" "Icon" -Value "$InstallPath\Icons\PowerShellPrompt.ico" -Type String -Force | Out-Null
+New-ItemProperty "HKCU:\Software\Classes\Directory\shell\Open ConEmu" "Icon" -Value "$InstallPath\Install\Icons\PowerShellPrompt.ico" -Type String -Force | Out-Null
 New-Item "HKCU:\Software\Classes\Directory\shell\Open ConEmu\command" -Value """$InstallPath\ConEmu\ConEmu.exe"" /cmd powershell.exe" -Type String -Force | Out-Null
 
 New-Item "HKCU:\Software\Classes\Drive\shell\Open ConEmu" -Force | Out-Null
-New-ItemProperty "HKCU:\Software\Classes\Drive\shell\Open ConEmu" "Icon" -Value "$InstallPath\Icons\PowerShellPrompt.ico" -Type String -Force | Out-Null
+New-ItemProperty "HKCU:\Software\Classes\Drive\shell\Open ConEmu" "Icon" -Value "$InstallPath\Install\Icons\PowerShellPrompt.ico" -Type String -Force | Out-Null
 New-Item "HKCU:\Software\Classes\Drive\shell\Open ConEmu\command" -Value """$InstallPath\ConEmu\ConEmu.exe"" /cmd powershell.exe" -Type String -Force | Out-Null
 
 
 "Configuring shell integration (ConEmu Embedded)..."
 New-Item "HKCU:\Software\Classes\*\shell\Open ConEmu (Embedded)" -Force | Out-Null
-New-ItemProperty "HKCU:\Software\Classes\*\shell\Open ConEmu (Embedded)" "Icon" -Value "$InstallPath\Icons\PowerShellPrompt.ico" -Type String -Force | Out-Null
+New-ItemProperty "HKCU:\Software\Classes\*\shell\Open ConEmu (Embedded)" "Icon" -Value "$InstallPath\Install\Icons\PowerShellPrompt.ico" -Type String -Force | Out-Null
 New-Item "HKCU:\Software\Classes\*\shell\Open ConEmu (Embedded)\command" -Value """$InstallPath\ConEmu\ConEmu.exe"" /inside /cmd powershell.exe" -Type String -Force | Out-Null
 
 New-Item "HKCU:\Software\Classes\Directory\Background\shell\Open ConEmu (Embedded)" -Force | Out-Null
-New-ItemProperty "HKCU:\Software\Classes\Directory\Background\shell\Open ConEmu (Embedded)" "Icon" -Value "$InstallPath\Icons\PowerShellPrompt.ico" -Type String -Force | Out-Null
+New-ItemProperty "HKCU:\Software\Classes\Directory\Background\shell\Open ConEmu (Embedded)" "Icon" -Value "$InstallPath\Install\Icons\PowerShellPrompt.ico" -Type String -Force | Out-Null
 New-Item "HKCU:\Software\Classes\Directory\Background\shell\Open ConEmu (Embedded)\command" -Value """$InstallPath\ConEmu\ConEmu.exe"" /inside /cmd powershell.exe" -Type String -Force | Out-Null
 
 New-Item "HKCU:\Software\Classes\Directory\shell\Open ConEmu (Embedded)" -Force | Out-Null
-New-ItemProperty "HKCU:\Software\Classes\Directory\shell\Open ConEmu (Embedded)" "Icon" -Value "$InstallPath\Icons\PowerShellPrompt.ico" -Type String -Force | Out-Null
+New-ItemProperty "HKCU:\Software\Classes\Directory\shell\Open ConEmu (Embedded)" "Icon" -Value "$InstallPath\Install\Icons\PowerShellPrompt.ico" -Type String -Force | Out-Null
 New-Item "HKCU:\Software\Classes\Directory\shell\Open ConEmu (Embedded)\command" -Value """$InstallPath\ConEmu\ConEmu.exe"" /inside /cmd powershell.exe" -Type String -Force | Out-Null
 
 New-Item "HKCU:\Software\Classes\Drive\shell\Open ConEmu (Embedded)" -Force | Out-Null
-New-ItemProperty "HKCU:\Software\Classes\Drive\shell\Open ConEmu (Embedded)" "Icon" -Value "$InstallPath\Icons\PowerShellPrompt.ico" -Type String -Force | Out-Null
+New-ItemProperty "HKCU:\Software\Classes\Drive\shell\Open ConEmu (Embedded)" "Icon" -Value "$InstallPath\Install\Icons\PowerShellPrompt.ico" -Type String -Force | Out-Null
 New-Item "HKCU:\Software\Classes\Drive\shell\Open ConEmu (Embedded)\command" -Value """$InstallPath\ConEmu\ConEmu.exe"" /inside /cmd powershell.exe" -Type String -Force | Out-Null
