2f4ad56da0cccd9a4550e1644d3517b8cb7042b4
Paul Marston <vai@vai-net.co.uk>
2014-02-09 23:46:13 +0000

    wip

--- Profile/Exports/Find-String.ps1
+++ Profile/Exports/Find-String.ps1
@@ -9,75 +9,47 @@ function Find-String {
     )
 
 	$maxFileSizeToSearchInBytes = 1024*1024 # 1mb
+	$liesOfContext = 2
 
-	Get-ChildItem -Path $path -Recurse | 
+	$priorPath = ""
+
+	$global:T = Get-ChildItem -Path $path -Recurse | 
 		? { $_.PSIsContainer -eq $false -and ($includeLargeFiles -or $_.Length -le $maxFileSizeToSearchInBytes) } | 
-		Select-String -Pattern $pattern -AllMatches -SimpleMatch:$(!$regex) -Context:$(?: {$showContext} {"3"} {$false}) |
-		Out-ColorMatchInfo
+		Select-String -Pattern $pattern -AllMatches -SimpleMatch:$(!$regex) -Context $liesOfContext #|
+# 		% {
+# 	        if ($priorPath -ne $_.Path) {
+#             	Write-Host ("`n{0}" -f $_.RelativePath($path)) -foregroundColor Green
+#             	$priorPath = $_.Path
+#         	} elseif ($showContext) {
+#                 Write-Host "--"
+#         	}
+
+#             if ($showContext -and $_.Context.DisplayPreContext) {
+#             	$lines = ($_.LineNumber - $_.Context.DisplayPreContext.Length)..($_.LineNumber - 1)
+#         		for ($i = 0; $i -lt $_.Context.DisplayPreContext.length; $i++) {
+#         			Write-Host ("{0}: {1}" -f $lines[$i], $_.Context.DisplayPreContext[$i])
+#         		}
+#             }
+# $global:t += $_
+#             Write-Host ("{0}: " -f $_.LineNumber) -NoNewLine
+#             $index = 0
+#             foreach ($match in $_.Matches) {
+#                 #Write-Host $_.Line.SubString($index, $match.Index - $index) -NoNewLine
+#                 #Write-Host $match.Value -ForegroundColor Black -BackgroundColor Yellow -NoNewLine
+#                 $index = $match.Index + $match.Length
+#             }
+#             Write-Host $_.Line.SubString($index) 
+
+# 			if ($showContext -and $_.Context.DisplayPostContext) {
+# 				$lines = ($_.LineNumber + 1)..($_.LineNumber + $_.Context.DisplayPostContext.Length)
+# 	        	for ($i = 0; $i -lt $_.Context.DisplayPostContext.length; $i++) {
+# 	        		Write-Host ("{0}: {1}" -f $lines[$i], $_.Context.DisplayPostContext[$i])
+# 	        	}
+# 	  		}
+# 	  	}
 }
 @{Function = "Find-String"}
 
-set-strictmode -off
-function Out-ColorMatchInfo {
-param ( 
-    [Parameter(Mandatory=$true, ValueFromPipeline=$true)] 
-    [Microsoft.PowerShell.Commands.MatchInfo] 
-    $match
-)
 #http://mohundro.com/blog/2009/06/12/find-stringps1---ack-for-powershell/
 #https://github.com/drmohundro/Find-String/blob/master/Find-String.psm1
 #http://answers.oreilly.com/topic/1989-how-to-search-a-file-for-text-or-a-pattern-in-windows-powershell/
-    begin {
-        $script:priorPath = ''
-        $script:hasContext = $false
-    }
-    process { 
-        function Write-PathOrSeparator($match) {
-            if ($script:priorPath -ne $match.Path) {
-                write-host ''
-                write-host $match.Path -foregroundColor Green
-                $script:priorPath = $match.Path
-            }
-            else {
-                if ($script:hasContext) { 
-                    write-host '--' 
-                }
-            }
-        }
-        Write-PathOrSeparator $match
-
-        function Write-HighlightedMatch($match) {
-            write-host "$($match.LineNumber):" -nonewline
-            $index = 0
-            foreach ($m in $match.Matches) {
-                write-host $match.Line.SubString($index, $m.Index - $index) -nonewline
-                write-host $m.Value -ForegroundColor Black -BackgroundColor Yellow -nonewline
-                $index = $m.Index + $m.Length
-            }
-            if ($index -lt $match.Line.Length) {
-                write-host $match.Line.SubString($index) -nonewline
-            }
-            write-host ''
-        }
-
-        function Write-ContextLines($context, $contextLines) {
-            if ($context.length -eq $null) {return}
-
-            $script:hasContext = $true
-            for ($i = 0; $i -lt $context.length; $i++) {
-                "$($contextLines[$i])- $($context[$i])"
-            }
-        }
-
-
-        $lines = ($match.LineNumber - $match.Context.DisplayPreContext.Length)..($match.LineNumber - 1)
-        Write-ContextLines $match.Context.DisplayPreContext $lines
-
-        Write-HighlightedMatch $match
-
-        $lines = ($match.LineNumber + 1)..($match.LineNumber + $match.Context.DisplayPostContext.Length)
-        Write-ContextLines $match.Context.DisplayPostContext $lines
-
-    }
-    end {}
-}
